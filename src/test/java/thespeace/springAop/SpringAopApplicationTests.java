11b18471a676276993b4369a4393c4393541e8bd
thespeace <thespeace1@gmail.com>
2024-06-20 16:57:15 +0900

    Initial commit

---
+++ src/test/java/thespeace/springAop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package thespeace.springAop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
